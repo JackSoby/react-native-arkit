78b2ad9dd6d1bdcd4a42bf3ff0a042a6da8197e8
Jack Sobocinski <sobojack@bowst.com>
2020-08-12 22:20:54 -0400

    add headerfile

--- ios/RCTARKit.h
+++ ios/RCTARKit.h
@@ -127,7 +127,10 @@ typedef void (^RCTARKitReject)(NSString *code, NSString *message, NSError *error
 #endif
 
 
-class swiftTest {
+
+
+
+@objc class swiftTest: NSString {
     print("WOW! this is all the way from swift!");
     return "WOW! this is all the way from swift!";
 }
